8f8c3a8434552d00d50890d8049a721b6be60f5e
getinthere <getinthere@naver.com>
2024-02-15 16:38:33 +0900

    준비땅

--- src/main/java/ex01/App1.java
+++ src/main/java/ex01/App1.java
@@ -1,11 +1,14 @@
 package ex01;
 
+import ex01.model.User;
+
 public class App1 {
     public static void main(String[] args) {
         // 1. DB에서 가져온 데이터 - Flat(평평) 하게 가져올 수 밖에 없다
         DBData1 dbData = new DBData1(1, "제목1", "내용1", 3, "love", "love@nate.com");
 
-        // 2. dbData를 ViewData에 옮기시오 - ORM
+        // 2. dbData1를 ViewData1에 옮기시오 - ORM
+
 
     }
 }
